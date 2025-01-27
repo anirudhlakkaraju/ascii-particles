ddbd6595ca6dcca06a5d1e823499ecc5868f7ac0
anirudhlakkaraju <lakanirudh123@gmail.com>
2025-01-26 20:36:43 -0600

    feat: add flag for particle effect type

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"particles/particles"
 	"time"
 )
@@ -40,8 +42,23 @@ var c = `               .........:--===++++======================++++===--:.....
 
 `
 
+var effects = map[string]particles.ASCII{
+	"steam": particles.AsciiSteam,
+	"fire":  particles.AsciiFire,
+}
+
 func main() {
-	coffee := particles.NewCoffee(61, 8, 9.0)
+
+	argType := flag.String("effect", "steam", "Specify the particle effect: 'steam' or 'fire'")
+	flag.Parse()
+
+	effect, ok := effects[*argType]
+	if !ok {
+		log.Fatalf("Invalid effect type: %s", *argType)
+	}
+
+	// Pour and enjoy!
+	coffee := particles.NewCoffee(61, 8, 9.0, effect)
 	coffee.Start()
 
 	timer := time.NewTicker(100 * time.Millisecond)
